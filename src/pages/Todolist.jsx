67ccf6ae9c8fc58dcf590b6eb43b27aa63919ec7
dlzagu <eodnsdlekd@naver.com>
2022-09-22 14:43:42 +0900

    Feat : 추가된 컴포넌트 import

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, memo } from "react";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { todoListState } from "../atom";
@@ -6,16 +6,14 @@ import TodoItem from "./TodoItem.jsx";
 import TodoItemCreator from "./TodoItemCreator.jsx";
 import Clock from "./Clock.jsx";
 import Weather from "./Weather.jsx";
+import Quote from "./Quotes.jsx";
+import images from "../dev/backGroundImg";
+import TodoListStats from "./TodoListStats.jsx";
 
 const TodoList = () => {
   const todoList = useRecoilValue(todoListState);
   const [bg, setBg] = useState("");
-  const images = [
-    "https://cdn.pixabay.com/photo/2016/02/18/20/02/seljalandsfoss-1207958_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2022/02/28/04/50/nature-7038455_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2021/11/02/21/57/prayer-6764197_960_720.jpg",
-  ];
-  const chosenImage = images[Math.floor(Math.random() * images.length)];
+
   useEffect(() => {
     setBg(chosenImage);
   }, [chosenImage]);
@@ -23,16 +21,18 @@ const TodoList = () => {
     <Wrapper bg={bg}>
       <ContentWrapper>
         <Weather />
+        <TodoListStats />
         <Clock></Clock>
         <TodoItemCreator />
         {todoList.map((todoItem) => (
           <TodoItem item={todoItem} key={todoItem.id} />
         ))}
+        <Quote />
       </ContentWrapper>
     </Wrapper>
   );
 };
-
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -48,9 +48,8 @@ const Wrapper = styled.div`
       "https://user-images.githubusercontent.com/59393359/74718667-0adb8a80-5276-11ea-8bc3-0e36c67cf28a.jpg"});
 `;
 const ContentWrapper = styled.div`
-  flex: 1;
-  max-width: 1024px;
+  width: 70rem;
   overflow: auto;
-  padding: 20px;
+  padding: 2rem;
 `;
 export default TodoList;
